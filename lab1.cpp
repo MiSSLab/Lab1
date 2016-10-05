50d4e1dade1de25c7c75ceeac82507fd97a7b4a2
humblehound <dejtabejz@gmail.com>
2016-10-06 01:14:37 +0200

    proper output format

--- lab1.cpp
+++ lab1.cpp
@@ -5,6 +5,29 @@
 
 using namespace std;
 
+void adjust_zeros(mpf_t number, string* result, int precision){
+
+	char* buffer = new char[300];
+	int n = gmp_sprintf(buffer, "%.*Ff", precision, number);
+
+	int index = n;
+	bool doRemove = true;
+
+	while(doRemove){
+		if(buffer[index-1] == '0'){
+			index--;
+		}else{
+			doRemove = false;
+		}
+
+	}
+	if(buffer[index-1] == '.'){
+		index--;
+	}
+	buffer[index+1] = '\n';
+	(*result).assign(buffer, index);
+
+}
 void average(mpf_t *array, int size, mpf_t mpf_size, mpf_t result) {
 
     for (int i = 0; i < size; i++) {
@@ -16,15 +39,16 @@ void average(mpf_t *array, int size, mpf_t mpf_size, mpf_t result) {
 
 void variance_period(mpf_t *array, int size, mpf_t mpf_size, mpf_t average, mpf_t variance, mpf_t period) {
 
-    mpf_t temp;
+    mpf_t temp, square;
     mpf_init(temp);
+    mpf_init(square);
     int periodLen = 0;
 
     for (int i = 0; i < size; i++) {
         // variance
         mpf_sub(temp, array[i], average);
-        mpf_pow_ui(temp, temp, 2);
-        mpf_add(variance, variance, temp);
+        mpf_pow_ui(square, temp, 2);
+        mpf_add(variance, variance, square);
 
         //period
         if (i != 0) {
@@ -43,7 +67,6 @@ void variance_period(mpf_t *array, int size, mpf_t mpf_size, mpf_t average, mpf_
     }
 
     mpf_set_si(period, periodLen);
-    mpf_clear(temp);
     mpf_div(variance, variance, mpf_size);
 
 }
@@ -75,13 +98,21 @@ int main() {
     average(numbers, num_count, num_count_mpf, average_result);
     variance_period(numbers, num_count, num_count_mpf, average_result, variance_result, period_result);
 
-    cout << average_result << endl;
-    cout << setprecision(16) << variance_result << endl;
+    string* adjusted = new string();
+    adjust_zeros(average_result, adjusted, 5);
+    cout << *adjusted << endl;
+
+    adjust_zeros(variance_result, adjusted, 5);
+    cout << *adjusted << endl;
+
+//    cout << setiosflags(ios::fixed);
+   // cout << fixed << setprecision(5) << average_result << endl;
+//    cout << setprecision(5) << variance_result << endl;
 
 //    gmp_printf (" %.*Ff \n", d, average_result);
 //    gmp_printf (" %.*Ff \n", d, variance_result);
 
-    cout << period_result << endl;
+    cout << setprecision(0) << period_result << endl;
 
     mpf_clear(average_result);
     mpf_clear(variance_result);
