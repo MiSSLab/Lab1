3b5e1ed2669978ab933db374546634aa8dba5d7b
humblehound <dejtabejz@gmail.com>
2016-10-05 14:57:59 +0200

    period, format output

--- lab1.cpp
+++ lab1.cpp
@@ -1,47 +1,51 @@
 #include <iostream>
 #include <gmp.h>
 #include <gmpxx.h>
+#include <iomanip>
 
 using namespace std;
 
-void average(mpf_t* array, int size, mpf_t mpf_size, mpf_t result){
+void average(mpf_t *array, int size, mpf_t mpf_size, mpf_t result) {
 
-    for(int i=0;i<size;i++){
+    for (int i = 0; i < size; i++) {
         mpf_add(result, result, array[i]);
     }
 
     mpf_div(result, result, mpf_size);
 }
 
-void variance_period(mpf_t* array, int size, mpf_t mpf_size, mpf_t average, mpf_t variance, mpf_t period){
+void variance_period(mpf_t *array, int size, mpf_t mpf_size, mpf_t average, mpf_t variance, mpf_t period) {
 
     mpf_t temp;
     mpf_init(temp);
+    int periodLen = 0;
 
-    for(int i=0;i<size;i++){
-
+    for (int i = 0; i < size; i++) {
+        // variance
         mpf_sub(temp, array[i], average);
         mpf_pow_ui(temp, temp, 2);
         mpf_add(variance, variance, temp);
+
+        //period
+        if (i != 0) {
+            if (periodLen != 0) {
+                if (mpf_cmp(array[i], array[i % periodLen]) != 0) {
+                    periodLen = i;
+                }
+            } else if (mpf_cmp(array[i], array[0]) == 0) {
+                periodLen = i;
+            }
+        }
     }
 
-    mpf_clear(temp);
+    if (periodLen == 0) {
+        periodLen = size;
+    }
 
+    mpf_set_si(period, periodLen);
+    mpf_clear(temp);
     mpf_div(variance, variance, mpf_size);
 
-}
-void period(mpf_t* array, int size, mpf_t sizempf, mpf_t period){
-
-	int periodLen = 1;
-	for(int i=1;i<size;i++)
-	{
-		if(array[i]!=array[i%(periodLen-1)]){
-			periodLen++;
-		}else{
-			periodLen=i;
-		}
-	}
-//	mpf_set_si(period, periodLen);
 }
 
 int main() {
@@ -50,12 +54,10 @@ int main() {
     long long int num_count;
     mpf_t num_count_mpf;
     mpf_t average_result, variance_result, period_result;
-    cout.precision(d);
-    ios::sync_with_stdio(false);
 
     // 2^24 numbers
-    mpf_t* numbers = new mpf_t[16777216];
-    for(int i=0;i<16777216;i++){
+    mpf_t *numbers = new mpf_t[16777216];
+    for (int i = 0; i < 16777216; i++) {
         mpf_init(numbers[i]);
     }
 
@@ -65,21 +67,20 @@ int main() {
     mpf_init(num_count_mpf);
     num_count = 0;
 
-    while(cin >> numbers[num_count]){
-	    num_count++;
+    while (cin >> numbers[num_count]) {
+        num_count++;
     }
-    
-   // while(gmp_scanf("%Ff", numbers[num_count]) != EOF){
-   //     num_count++;
-   // }
-   
+
     mpf_set_si(num_count_mpf, num_count);
     average(numbers, num_count, num_count_mpf, average_result);
     variance_period(numbers, num_count, num_count_mpf, average_result, variance_result, period_result);
-    period(numbers, num_count, num_count_mpf, period_result);
 
     cout << average_result << endl;
-    cout << variance_result << endl;
+    cout << setprecision(16) << variance_result << endl;
+
+//    gmp_printf (" %.*Ff \n", d, average_result);
+//    gmp_printf (" %.*Ff \n", d, variance_result);
+
     cout << period_result << endl;
 
     mpf_clear(average_result);
